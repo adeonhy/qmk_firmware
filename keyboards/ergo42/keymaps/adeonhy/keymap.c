154b6cbeee784d89dcedcf8acc4b97500cbd08d5
Naoya Ishisaki <adeonhy@gmail.com>
2018-05-08 20:39:37 +0900

    wip

--- keyboards/ergo42/keymaps/adeonhy/keymap.c
+++ keyboards/ergo42/keymaps/adeonhy/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------.   ,------------------------------------------------.
  * | ESC  | Tab  |   Q  |   W  |   E  |   R  |  T   |   |  Y   |   U  |   I  |   O  |   P  |   [  | Bksp |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Del  | LCtrl|   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   '  | Enter|
+ * |  [   |]/Ctrl|   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   '  | Enter|
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | SYMB | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |   \  | ESC  |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -29,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = KEYMAP( \
   KC_ESC,   KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
-  KC_DELT,  KC_LCTL, KC_A,     KC_S,   KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-  MO(SYMB), KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_ESC, \
-  KC_LEFT,  KC_UP,   MO(META), MO(SYMB), KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_RGUI, KC_DOWN, KC_RGHT,KC_GRV,  KC_MINS, KC_EQL \
+  KC_LPRN,  CTL_T(KC_RPRN), KC_A,     KC_S,   KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
+  LT(SYMB,KC_LPRN), KC_RSPC, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_ESC, \
+  KC_LBRC,  KC_RBRC,   MO(META), MO(SYMB), KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_RGUI, KC_DOWN, KC_RGHT,KC_GRV,  KC_MINS, KC_EQL \
 ),
 
 /* META
