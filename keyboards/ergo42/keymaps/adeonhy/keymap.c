5d2e26d0a3d40856f51fe6bbc246969ad3463cfb
Naoya Matsuyama <adeonhy@gmail.com>
2018-05-23 10:57:46 +0900

    change key map

--- keyboards/ergo42/keymaps/adeonhy/keymap.c
+++ keyboards/ergo42/keymaps/adeonhy/keymap.c
@@ -17,21 +17,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | ESC  | Tab  |   Q  |   W  |   E  |   R  |  T   |   |  Y   |   U  |   I  |   O  |   P  | ESC  | Bksp |
+ * | ESC  | Tab  |   Q  |   W  |   E  |   R  |  T   |   |  Y   |   U  |   I  |   O  |   P  |  -   | Bksp |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * |      |  Ctrl|   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   '  | Enter|
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * |      | (LSft|   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |   \  | RSft)|
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | [    |  ]   | META |  ESC | LAlt | LGUI |Space |   |Space | RGUI | DOWN | RIGHT|   `  |   -  |   =  |
+ * | [    |  ]   | META |  ESC | LAlt | LGUI |Space |   |Space | RGUI | ESC  |      |   `  |   -  |   =  |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_ESC,   KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_ESC, KC_BSPC, \
+  KC_ESC,   KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS, KC_BSPC, \
   _______,  KC_LCTL, KC_A,     KC_S,   KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
   _______,  KC_LSPO, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_RSPC, \
-  KC_LBRC,  KC_RBRC, MO(META), MO(SYMB), KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), LT(META, KC_SPC), KC_RGUI, KC_DOWN, KC_RGHT,KC_GRV,  KC_MINS, KC_EQL \
+  KC_LBRC,  KC_RBRC, MO(META), MO(SYMB), KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), LT(META, KC_SPC), KC_RGUI, KC_ESC, KC_RGHT,KC_GRV,  KC_MINS, KC_EQL \
 ),
 
 /* META
@@ -57,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------.   ,------------------------------------------------.
  * | RESET|      |   !  |   @  |   #  |   $  |  %   |   |  ^   |   &  |   *  |   (  |   )  |      | Bksp |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   |      |      |      |      |      |      | Enter|
- * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
+ * | Del  |      |      |      |      |      |      |   | LEFT | DOWN | UP   | RIGHT|      |      | Enter|
+ * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * | SYMB |      |      |      |      |      |      |   |      |      |      |      |      |  UP  | ESC  |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * | META | LCtrl|   `  |   \  | LAlt | LGUI |Space |   |Space | RGUI | RALT |      | LEFT | DOWN | RIGHT|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [SYMB] = KEYMAP( \
   _______, _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, _______, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   _______, \
   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT \
 )
