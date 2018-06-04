e76455a6bfecc92e729d5596e168a965134fc715
Naoya Matsuyama <adeonhy@gmail.com>
2018-06-04 10:07:36 +0900

    Update keymap

--- keyboards/ergo42/keymaps/samus/keymap.c
+++ keyboards/ergo42/keymaps/samus/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  [   |   |  ]   |   N  |   M  |   ,  |   .  |   /  |  \   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * |      |      |      |LALT  |LGUI/ |Space/|Enter |   |Back  |Space/| RGUI | ESC/ | `    |  -   |  =   |
+ * |  =   |  -   |  `   |LALT  |LGUI/ |Space/|Enter |   |Back  |Space/| RGUI | ESC/ | Copy | Paste| Enter|
  * |      |      |      |      |      |SYMB  |      |   |Space |META  |      | Alt  |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,             S(KC_COMM),    S(KC_DOT),  KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
   KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,             S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,             KC_LBRC,       KC_RBRC,    KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_BSLS), \
-  XXXXXXX,  XXXXXXX, XXXXXXX,KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), KC_ENT,        KC_BSPC,    LT(META, KC_SPC), KC_ESC,  KC_RGUI,  KC_GRV,   KC_MINS,  KC_EQL \
+  KC_EQL,   KC_MINS, KC_GRV, KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), KC_ENT,        KC_BSPC,    LT(META, KC_SPC), KC_ESC,  KC_RGUI,  LGUI(KC_C), LGUI(KC_V), KC_ENT \
 ),
 
 /* META
