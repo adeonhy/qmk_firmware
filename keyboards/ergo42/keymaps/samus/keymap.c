74ad555aa2ca45452cf991e8cdefe240add745e3
Naoya Matsuyama <adeonhy@gmail.com>
2018-07-03 11:00:43 +0900

    Add NUM layer,

--- keyboards/ergo42/keymaps/samus/keymap.c
+++ keyboards/ergo42/keymaps/samus/keymap.c
@@ -7,7 +7,7 @@ extern keymap_config_t keymap_config;
 #define BASE 0
 #define META 1
 #define SYMB 2
-#define GAME 3
+#define NUM 3
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  [   |   |  ]   |   N  |   M  |   ,  |   .  |   /  |  \   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * |  =   |  -   |  `   |LALT  |LGUI/ |Space/|Enter |   |Back  |Space/| RGUI | ESC/ | Copy | Paste| Enter|
+ * |=/NUM |  -   |  `   |LALT  |LGUI/ |Space/|Enter |   |Back  |Space/| RGUI | ESC/ | Copy | Paste| Enter|
  * |      |      |      |      |      |SYMB  |      |   |Space |META  |      | Alt  |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,             S(KC_COMM),    S(KC_DOT),  KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
   KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,             S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,             KC_LBRC,       KC_RBRC,    KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_BSLS), \
-  KC_EQL,   KC_MINS, KC_GRV, KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), KC_ENT,        KC_BSPC,    LT(META, KC_SPC), KC_ESC,  KC_RGUI,  LGUI(KC_C), LGUI(KC_V), KC_ENT \
+  LT(NUM, KC_EQL),   KC_MINS, KC_GRV, KC_LALT, KC_LGUI, LT(SYMB, KC_SPC), KC_ENT,        KC_BSPC,    LT(META, KC_SPC), KC_ESC,  KC_RGUI,  LGUI(KC_C), LGUI(KC_V), KC_ENT \
 ),
 
 /* META
@@ -75,21 +75,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* GAME
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   7  |   8  |   9  |  @   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
+ * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   4  |   5  |   6  |  :   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
+ * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   0  |   1  |   2  |   3  |\/Sft |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * | Ctrl | GUI  |  App |PrtSc | ESC  |Space |Tab   |   |Back  |Enter | Del  |PrtSc |=>GAME|=>SYMB|  \   |
  * |      |      |      |      |      |      |      |   |Space |      |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
-[GAME] = KEYMAP( \
-  KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC, \
-  KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,   KC_G,   S(KC_8),    S(KC_9),    KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, SFT_T(KC_RO), \
-  KC_LCTRL, KC_LGUI, KC_APP, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, KC_JYEN \
+[NUM] = KEYMAP( \
+  KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_U,    KC_7,     KC_8,    KC_9,    KC_LBRC, \
+  KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,   KC_G,   S(KC_8),    S(KC_9),    KC_H,   KC_J,    KC_4,     KC_5,    KC_6,    KC_QUOT, \
+  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_0,    KC_1,     KC_2,    KC_3, SFT_T(KC_RO), \
+  _______, KC_LGUI, KC_APP, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, KC_JYEN \
 )
 
 };
