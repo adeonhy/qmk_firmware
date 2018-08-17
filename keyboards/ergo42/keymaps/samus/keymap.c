e0bd21e123a5e63845e2e194441a7505cf624441
Naoya Matsuyama <adeonhy@gmail.com>
2018-08-17 10:34:38 +0900

    add ergodash keymap

--- keyboards/ergo42/keymaps/samus/keymap.c
+++ keyboards/ergo42/keymaps/samus/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[BASE] = KEYMAP( \
+[BASE] = LAYOUT( \
   KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,             S(KC_COMM),    S(KC_DOT),  KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
   KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,             S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,             KC_LBRC,       KC_RBRC,    KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_BSLS), \
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[META] = KEYMAP( \
+[META] = LAYOUT( \
   RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   _______, _______, _______, _______, _______, _______, S(KC_LBRC), S(KC_RBRC), _______, _______, _______, KC_F11,  KC_F12,  _______, \
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[SYMB] = KEYMAP( \
+[SYMB] = LAYOUT( \
   RESET,   S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______, S(KC_6), S(KC_7), S(KC_8), S(KC_9),  S(KC_0), _______, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______, _______ , \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_RBRC), XXXXXXX, XXXXXXX, _______, _______,  KC_UP,   _______, \
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |   |Space |      |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
-[NUM] = KEYMAP( \
+[NUM] = LAYOUT( \
   KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_7,    KC_8,     KC_9,    _______,    KC_WH_U, \
   KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,   KC_G,   S(KC_8),    S(KC_9),    KC_H,   KC_4,    KC_5,     KC_6,    KC_WH_L,    KC_WH_R, \
   KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_0,   KC_1,    KC_2,     KC_3,    KC_MS_U,    KC_WH_D, \
